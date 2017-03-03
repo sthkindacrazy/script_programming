029fca7f0430b16b2d757247c08bfd1cd1d8c88d
sthkindacrazy <shinjoohyung25@gmail.com>
2017-03-03 22:37:15 +0900

    created checkMemories.py

---
+++ checkMemories.py
@@ -0,0 +1,20 @@
+# -*- python3 -*-
+""" Among information given after typing system info in Window cmd,
+	this script will keep check memory, both real and virtual and
+	record in a file """
+
+import subprocess
+import re
+import time
+
+filedirpath = "C:\\Desktop\\folder"
+
+def checkMemory():
+	systemres = subprocess.getoutput(["systeminfo"])
+	# to get rid of , ex) 1,576 -> 1576
+	systemres = systemres.replace(",", "")
+	
+	#use regex to pull out memories only
+	pattern = re.compile('\d+MB')
+	
+	memorylist = pattern.findall(systemres)
