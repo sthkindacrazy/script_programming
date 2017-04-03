b050d6d0749e52b25f2f82392c6ede4587de55c9
Shin <shinjoohyung25@gmail.com>
2017-04-03 23:27:18 +0900

    update1
    update1

--- enc_module.py
+++ enc_module.py
@@ -3,3 +3,14 @@
 from os import urandom
 from hashlib import md5
 from Crypto.Cipher import AES
+
+def derive_keyand_iv(password, salt, key_length, iv_length):
+  d = d_i = b''
+  while len(d) < key_length + iv_length:
+    d_i = md5(d_i + str.encode(password) + salt).digest()
+    d += d_i
+  return d[:key_length, d[key_length:key_length + iv_length]
+
+def encrypt_file(infile, outfile, password, salt_header='', key_length = 32):
+  bs = AES.block_size
+  
